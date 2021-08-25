6cca978605eea4f7f8c7b161aa3cd08792548422
Nathaniel Tornow <nathaniel.tornow@tum.de>
2021-08-25 14:21:23 +0200

    Shard proto

--- shard/shard.go
+++ shard/shard.go
@@ -1,13 +1,41 @@
 package shard
 
-import "github.com/nathanieltornow/PMLog/storage"
+import (
+	"context"
+	"fmt"
+	shardpb "github.com/nathanieltornow/PMLog/shard/shardpb"
+	"github.com/nathanieltornow/PMLog/storage"
+	"sync"
+)
 
 type Shard struct {
+	shardpb.UnimplementedShardServer
+
+	head    bool
+	tail    bool
+	stateMu sync.Mutex
+
 	log storage.Log
 }
 
-func NewShard(log storage.Log) (*Shard, error) {
+func NewShard(log storage.Log, head, tail bool) (*Shard, error) {
 	shard := new(Shard)
 	shard.log = log
 	return shard, nil
+}
+
+func (s *Shard) Append(_ context.Context, req *shardpb.AppendRequest) (*shardpb.OrderResponse, error) {
+	s.stateMu.Lock()
+	head := s.head
+	s.stateMu.Unlock()
+	if !head {
+		return nil, fmt.Errorf("shard isn't head, no append possible")
+	}
+
+	return nil, nil
+}
+
+func (s *Shard) Replicate(stream shardpb.Shard_ReplicateServer) error {
+
+	return nil
 }
