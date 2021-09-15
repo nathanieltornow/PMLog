f82fad6b4a2bbc05eaff23bbfea307e7362de52e
Nathaniel Tornow <nathaniel.tornow@tum.de>
2021-09-15 10:48:07 +0200

    shared log app implementation

--- shared_log/app.go
+++ shared_log/app.go
@@ -4,16 +4,33 @@ import (
 	frame "github.com/nathanieltornow/PMLog/order_repl_framework"
 )
 
-type waitingRecord struct {
-	record string
-	gsn    chan uint64
+type newRecord struct {
+	findToken uint64
+	record    string
+	color     uint32
+	gsn       chan uint64
 }
 
 func (sl *SharedLog) MakeCommitRequests(ch chan *frame.CommitRequest) error {
+	for newRec := range sl.newAppends {
+		comReq := &frame.CommitRequest{
+			Color:     newRec.color,
+			Content:   newRec.record,
+			FindToken: newRec.findToken,
+		}
+		ch <- comReq
+	}
 	return nil
 }
 
-func (sl *SharedLog) Prepare(localToken uint64, color uint32, content string) error {
+func (sl *SharedLog) Prepare(localToken uint64, color uint32, content string, findToken uint64) error {
+	// TODO Color
+	if err := sl.log.Append(content, localToken); err != nil {
+		return err
+	}
+	sl.localToFindTokenMu.Lock()
+	sl.localToFindToken[localToken] = findToken
+	sl.localToFindTokenMu.Unlock()
 	return nil
 }
 
@@ -21,6 +38,15 @@ func (sl *SharedLog) IsPrepared(localToken uint64) bool {
 	return true
 }
 
-func (sl *SharedLog) Commit(localToken uint64, color uint32, uint642 uint64) error {
+func (sl *SharedLog) Commit(localToken uint64, color uint32, globalToken uint64) error {
+	// TODO color
+	if err := sl.log.Commit(localToken, globalToken); err != nil {
+		return err
+	}
+
+	sl.pendingAppendsMu.Lock()
+	sl.pendingAppends[localToken] <- globalToken
+	delete(sl.pendingAppends, localToken)
+	sl.pendingAppendsMu.Unlock()
 	return nil
 }
--- shared_log/shared_log.go
+++ shared_log/shared_log.go
@@ -2,30 +2,66 @@ package shared_log
 
 import (
 	"context"
+	"fmt"
 	pb "github.com/nathanieltornow/PMLog/shared_log/shared_logpb"
 	"github.com/nathanieltornow/PMLog/shared_log/storage"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"net"
+	"sync"
+	"sync/atomic"
 )
 
 type SharedLog struct {
 	pb.UnimplementedSharedLogServer
 	log storage.Log
 
-	newAppends chan *pb.AppendRequest
+	tokenCtr         uint64
+	pendingAppends   map[uint64]chan uint64
+	pendingAppendsMu sync.Mutex
 
-	pendingCommits map[uint64]*waitingRecord
+	localToFindToken   map[uint64]uint64
+	localToFindTokenMu sync.Mutex
+
+	newAppends chan *newRecord
 }
 
 func NewSharedLog(log storage.Log) (*SharedLog, error) {
 	sl := new(SharedLog)
 	sl.log = log
-	sl.newAppends = make(chan *pb.AppendRequest, 1024)
+	sl.pendingAppends = make(map[uint64]chan uint64)
+	sl.newAppends = make(chan *newRecord, 1024)
+	sl.localToFindToken = make(map[uint64]uint64)
 	return sl, nil
 }
 
-func (sl *SharedLog) Append(_ context.Context, req *pb.AppendRequest) (*pb.AppendResponse, error) {
-	sl.newAppends <- req
+func (sl *SharedLog) Start(ipAddr string) error {
+	lis, err := net.Listen("tcp", ipAddr)
+	if err != nil {
+		return err
+	}
+	server := grpc.NewServer()
+	pb.RegisterSharedLogServer(server, sl)
 
-	return nil, nil
+	logrus.Infoln("Starting Shared log")
+	if err := server.Serve(lis); err != nil {
+		return fmt.Errorf("failed to start shared log: %v", err)
+	}
+	return nil
+}
+
+func (sl *SharedLog) Append(_ context.Context, req *pb.AppendRequest) (*pb.AppendResponse, error) {
+	newToken := atomic.AddUint64(&sl.tokenCtr, 1)
+	waitingGsn := make(chan uint64)
+	sl.pendingAppendsMu.Lock()
+	sl.pendingAppends[newToken] = waitingGsn
+	sl.pendingAppendsMu.Unlock()
+
+	sl.newAppends <- &newRecord{findToken: newToken, color: req.Color, gsn: waitingGsn, record: req.Record}
+
+	// wait for the global-sequence number
+	gsn := <-waitingGsn
+	return &pb.AppendResponse{Gsn: gsn}, nil
 }
 
 func (sl *SharedLog) Read(req *pb.ReadRequest, stream pb.SharedLog_ReadServer) error {
