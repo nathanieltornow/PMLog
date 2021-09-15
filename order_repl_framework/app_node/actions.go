f63abd800b4c26f5f5622fe6dcc2616fb6ba3242
Nathaniel Tornow <nathaniel.tornow@tum.de>
2021-09-15 11:43:37 +0200

    refactor

--- order_repl_framework/app_node/actions.go
+++ order_repl_framework/app_node/actions.go
@@ -41,17 +41,19 @@ func (n *Node) handleAppCommitRequests() {
 		}
 	}()
 	for comReq := range comReqCh {
-		prepMsg := &nodepb.Prep{
-			LocalToken: n.getNewLocalToken(),
-			Color:      comReq.Color,
-			Content:    comReq.Content,
-		}
-		if err := n.app.Prepare(prepMsg.LocalToken, prepMsg.Color, prepMsg.Content, comReq.FindToken); err != nil {
+		newToken := n.getNewLocalToken()
+
+		if err := n.app.Prepare(newToken, comReq.Color, comReq.Content, comReq.FindToken); err != nil {
 			return
 		}
 		// put into channel to be broadcasted to other nodes and send orderrequest
-		n.prepCh <- prepMsg
-		n.orderReqCh <- &seqpb.OrderRequest{Lsn: prepMsg.LocalToken, Color: prepMsg.Color, OriginColor: n.color}
+
+		n.prepCh <- &nodepb.Prep{
+			LocalToken: newToken,
+			Color:      comReq.Color,
+			Content:    comReq.Content,
+		}
+		n.orderReqCh <- &seqpb.OrderRequest{Lsn: newToken, Color: comReq.Color, OriginColor: n.color}
 	}
 }
 
