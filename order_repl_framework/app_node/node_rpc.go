00acb90be1fa852da0a910a5c6b83c37b070017c
Nathaniel Tornow <nathaniel.tornow@tum.de>
2021-09-18 02:11:16 +0200

    Version with eventual consistency

--- order_repl_framework/app_node/node_rpc.go
+++ order_repl_framework/app_node/node_rpc.go
@@ -15,30 +15,3 @@ func (n *Node) Prepare(stream nodepb.Node_PrepareServer) error {
 		n.prepMan.prepare(prepMsg, 0)
 	}
 }
-
-func (n *Node) GetAcks(req *nodepb.AckReq, stream nodepb.Node_GetAcksServer) error {
-	ackCh := make(chan *nodepb.Ack, 512)
-
-	n.ackChsMu.Lock()
-	n.ackChs[req.NodeID] = ackCh
-	n.ackChsMu.Unlock()
-
-	n.comMan.addPeer()
-
-	for ack := range ackCh {
-		if err := stream.Send(ack); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (n *Node) Commit(stream nodepb.Node_CommitServer) error {
-	for {
-		comMsg, err := stream.Recv()
-		if err != nil {
-			return fmt.Errorf("failed to receive com-msg: %v", err)
-		}
-		n.comMan.receiveCom(comMsg)
-	}
-}
