99ac1cd21d7175ad85efab4b1e9c3b652d5c9aca
Nathaniel Tornow <nathaniel.tornow@tum.de>
2021-10-12 09:37:41 +0200

    Mem_log_go to use normal maps

--- shared_log/storage/mem_log_go/mem_log_go.go
+++ shared_log/storage/mem_log_go/mem_log_go.go
@@ -6,8 +6,10 @@ import (
 )
 
 type MemLogGo struct {
-	lsnToRecord sync.Map
-	gsnToRecord sync.Map
+	lsnMu       sync.Mutex
+	lsnToRecord map[uint64]string
+	gsnMu       sync.RWMutex
+	gsnToRecord map[uint64]string
 }
 
 func NewMemLogGo() *MemLogGo {
@@ -16,37 +18,47 @@ func NewMemLogGo() *MemLogGo {
 }
 
 func (mlg *MemLogGo) Append(record string, lsn uint64) error {
-	mlg.lsnToRecord.Store(lsn, record)
+	mlg.lsnMu.Lock()
+	mlg.lsnToRecord[lsn] = record
+	mlg.lsnMu.Unlock()
 	return nil
 }
 
 // Commit commits the records which is stored on local-sequence-number lsn with the global-sequence-number gsn on
 // on the log of color. The record can be committed for multiple colors.
 func (mlg *MemLogGo) Commit(lsn uint64, gsn uint64) error {
-	rec, ok := mlg.lsnToRecord.LoadAndDelete(lsn)
+	mlg.lsnMu.Lock()
+	rec, ok := mlg.lsnToRecord[lsn]
+	delete(mlg.lsnToRecord, lsn)
+	mlg.lsnMu.Unlock()
 	if !ok {
 		return fmt.Errorf("couldn't find record")
 	}
-	mlg.gsnToRecord.Store(gsn, rec)
+	mlg.gsnMu.Lock()
+	mlg.gsnToRecord[gsn] = rec
+	mlg.gsnMu.Unlock()
 	return nil
 }
 
 // Read returns the record stored on global-sequence-number gsn on the log of color
 func (mlg *MemLogGo) Read(gsn uint64) (string, error) {
-	rec, ok := mlg.gsnToRecord.Load(gsn)
+	mlg.gsnMu.RLock()
+	defer mlg.gsnMu.RUnlock()
+	rec, ok := mlg.gsnToRecord[gsn]
 	if !ok {
 		return "", fmt.Errorf("failed to find record")
 	}
-	return rec.(string), nil
+	return rec, nil
 }
 
 // Trim deletes all records of the log of color before global-sequence-number gsn
 func (mlg *MemLogGo) Trim(gsn uint64) error {
-	mlg.gsnToRecord.Range(func(key, value interface{}) bool {
-		if key.(uint64) < gsn {
-			mlg.gsnToRecord.Delete(key)
+	mlg.gsnMu.Lock()
+	for recGsn := range mlg.gsnToRecord {
+		if recGsn < gsn {
+			delete(mlg.gsnToRecord, recGsn)
 		}
-		return true
-	})
+	}
+	mlg.gsnMu.Unlock()
 	return nil
 }
