d157aa469dbff3f47cb24cbd024a3789305cbf66
dgiantsidi <dimitra.giantsidi@gmail.com>
2022-07-13 11:50:33 +0000

    minor refactoring #2

--- storage/pm_log/cPMLog.cpp
+++ storage/pm_log/cPMLog.cpp
@@ -2,42 +2,42 @@
 #include "PMLog.h"
 
 void PMLogFree(PMLog log) {
-    cppPMLog *cppLog = (cppPMLog *) log;
+	cppPMLog *cppLog = reinterpret_cast<cppPMLog *>(log);
 
-    delete cppLog;
+	delete cppLog;
 }
 
 PMLog startUp() {
-	return (PMLog) cppStartUp();
+	return static_cast<PMLog>(cppStartUp());
 }
 
 void finalize(PMLog log) {
 	persistent_ptr<cppPMLog> cppLog = persistent_ptr<cppPMLog>((pmem::detail::sp_element<cppPMLog>::type *) log);
-	
+
 	cppFinalize(cppLog);
 }
 
 int cAppend(PMLog log, const char* record, uint64_t lsn) {
-    persistent_ptr<cppPMLog> cppLog = persistent_ptr<cppPMLog>((pmem::detail::sp_element<cppPMLog>::type *) log);
-	
-    return cppLog->Append(record, lsn);
+	persistent_ptr<cppPMLog> cppLog = persistent_ptr<cppPMLog>((pmem::detail::sp_element<cppPMLog>::type *) log);
+
+	return cppLog->Append(record, lsn);
 }
 
 int cCommit(PMLog log, uint64_t lsn, uint64_t gsn){
-    persistent_ptr<cppPMLog> cppLog = persistent_ptr<cppPMLog>((pmem::detail::sp_element<cppPMLog>::type *) log);
+	persistent_ptr<cppPMLog> cppLog = persistent_ptr<cppPMLog>((pmem::detail::sp_element<cppPMLog>::type *) log);
 
-    return cppLog->Commit(lsn, gsn);
+	return cppLog->Commit(lsn, gsn);
 }
 
 const char *cRead(PMLog log, uint64_t gsn, void* next_gsn) {
-    persistent_ptr<cppPMLog> cppLog = persistent_ptr<cppPMLog>((pmem::detail::sp_element<cppPMLog>::type *) log);
-	uint64_t *nxtgsn = (uint64_t *) next_gsn;
-	
-    return cppLog->Read(gsn, nxtgsn);
+	persistent_ptr<cppPMLog> cppLog = persistent_ptr<cppPMLog>((pmem::detail::sp_element<cppPMLog>::type *) log);
+	uint64_t *nxtgsn = reinterpret_cast<uint64_t *>(next_gsn);
+
+	return cppLog->Read(gsn, nxtgsn);
 }
 
 int cTrim(PMLog log, uint64_t gsn) {
-    persistent_ptr<cppPMLog> cppLog = persistent_ptr<cppPMLog>((pmem::detail::sp_element<cppPMLog>::type *) log);
+	persistent_ptr<cppPMLog> cppLog = persistent_ptr<cppPMLog>((pmem::detail::sp_element<cppPMLog>::type *) log);
 
-    return cppLog->Trim(gsn);
+	return cppLog->Trim(gsn);
 }
