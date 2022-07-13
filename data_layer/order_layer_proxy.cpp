89df2e98f6a45e43e37496f9fc8d5f1647371693
Nathaniel Tornow <nathaniel.tornow@tum.de>
2022-07-13 11:51:58 +0200

    better order interface

--- data_layer/order_layer_proxy.cpp
+++ data_layer/order_layer_proxy.cpp
@@ -1,7 +1,3 @@
-#include "order_layer_proxy.h"
-
 #include <string>
 
-OrderLayerProxy::OrderLayerProxy(std::string order_ip) {}
-
-seqnum_t OrderLayerProxy::get_order(color_t color, seqnum_t lsn) {}
+// TODO implement orderlayer proxy with GRPC
--- data_layer/replica.cpp
+++ data_layer/replica.cpp
@@ -2,16 +2,20 @@
 
 namespace data_layer {
 
-Replica::Replica(IStorage storage, OrderLayerProxy order_layer)
-    : storage{storage}, order_layer{order_layer} {
-    // TODO
-}
+Replica::Replica(IStorage storage,
+                 order_layer_proxy::OrderLayerProxy order_layer)
+    : storage{storage}, order_layer{order_layer} {}
 
 AppendResponse Replica::append(AppendRequest app_req) {
     // TODO
 }
 
 ReadResponse Replica::read(ReadRequest read_req) {
+    // TODO
+}
+
+void Replica::receive_order_response(seqnum_t token,
+                                     seqnum_t gsn) {
     // TODO
 }
 
